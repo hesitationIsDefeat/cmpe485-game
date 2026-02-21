a45bd864d68eef336a19ecb1540425166f8a666c
hesitationIsDefeat <onat@192.168.1.6>
2026-02-21 16:33:49 +0300

    feat(script): Update MoveobleObject script to add force to the object every frame

--- Assets/Scripts/MoveableObject.cs
+++ Assets/Scripts/MoveableObject.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MoveableObject : MonoBehaviour
 {
+    private Rigidbody rigidBody;
+
+    public Vector3 addedForce = new Vector3(0, 10f, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.AddForce(addedForce);
     }
 }
