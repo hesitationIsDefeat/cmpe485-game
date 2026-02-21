2282525e51c0d7c780490c659512bae10ed02133
hesitationIsDefeat <onat@192.168.1.6>
2026-02-21 16:53:51 +0300

    feat(script): Add code to Spawner script to trigger object spawn
    Closes #10

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject objectToSpawn;
+
+    public Vector3 spawnPosition = new Vector3(-2, 3, -2);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S)) {
+            SpawnObject();
+        }
+    }
+
+    void SpawnObject() {
+        if (objectToSpawn == null) return;
+
+        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
